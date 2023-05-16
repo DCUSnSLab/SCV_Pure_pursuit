af275aac89083b94ba2e34e5f976e1101975bacc
DcuDongHyun <oksally@cu.ac.kr>
2023-05-16 15:11:53 +0900

    [AD-165] 시뮬레이션 토픽 기반 차량 현재 위치 추정

--- src/pure_pursuit_erp42.py
+++ src/pure_pursuit_erp42.py
@@ -1,13 +1,18 @@
+import geometry_msgs.msg
 import rospy
 import time
 import numpy as np
 
+import scv_control_controller
+from moraictrl import moraiCtrl
+import math
 from geometry_msgs.msg import Point
 from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_msgs.msg import Marker, MarkerArray
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Float64, ColorRGBA, Header
 from morai_msgs.msg import CtrlCmd
+from morai_msgs.msg import EgoVehicleStatus
 from tf.transformations import euler_from_quaternion
 
 class PurePursuit:
@@ -41,24 +46,16 @@ class PurePursuit:
         # Calculate the lookahead point
 
         closest_current_sub = [closest_point.x - current_pose[0], closest_point.y - current_pose[1]]
-        # print("closest")
-        # print(closest_current_sub)
-        
-        # print(closest_point)
-        # print(closest_distance)
-        
         refined_closest_point = [closest_point.x, closest_point.y]
         closest_distance_list = [closest_distance, closest_distance]
-        
+
         lookahead_distance_list = [self.lookahead_distance, self.lookahead_distance]
-        
         distance_mul = [lookahead_distance_list[i] * closest_current_sub[i] for i in range(len(closest_current_sub))]
-        
         distance_div = [distance_mul[i] / closest_distance_list[i] for i in range(len(closest_distance_list))]
 
         # lookahead_point = closest_point + self.lookahead_distance * (closest_point - current_pose[:2]) / closest_distance # Original code
         lookahead_point = [distance_div[i] + refined_closest_point[i] for i in range(len(refined_closest_point))]
-        
+
         tmp_marker = Marker()
 
         tmp_point = Point()
@@ -78,8 +75,8 @@ class PurePursuit:
         tmp_marker.scale.z = 0.5
 
         tmp_marker.color.r = 0.5
-        tmp_marker.color.b = 0.5
-        tmp_marker.color.a = 0.8
+        tmp_marker.color.b = 0.0
+        tmp_marker.color.a = 0.0
 
         tmp_marker.points[0].x = lookahead_point[0]
         tmp_marker.points[0].y = lookahead_point[1]
@@ -90,29 +87,20 @@ class PurePursuit:
 
         # Calculate the steering angle
         steering_angle = np.arctan2(lookahead_point[1] - current_pose[1], lookahead_point[0] - current_pose[0])
-        
-        print("steer :", steering_angle)
-        if steering_angle < 0:
-            if current_pose[2] < 0:
-                steering_angle = steering_angle - current_pose[2]
-            else:
-                steering_angle = steering_angle - current_pose[2]
-        elif steering_angle >= 0:
-            if current_pose[2] < 0:
-                steering_angle = steering_angle + current_pose[2]
-            else:
-                steering_angle = steering_angle - current_pose[2]
-        
-        # steering_angle = np.abs(steering_angle) + np.abs(current_pose[2]) - 6.28
-            
-        #print("steering_angle")
-        #print(steering_angle)
+
+        yaw_diff = steering_angle - current_pose[2]
+
+        # Adjust the steering angle based on the yaw difference
+        if yaw_diff > np.pi:
+            yaw_diff -= 2 * np.pi
+        elif yaw_diff < -np.pi:
+            yaw_diff += 2 * np.pi
+
+        # Set the steering angle as the adjusted yaw difference
+        steering_angle = yaw_diff
 
         # Limit the steering angle
-        print("yaw :", current_pose[2])
-        print("steer - yaw :", steering_angle)
-        # steering_angle = np.clip(steering_angle, -self.max_steering_angle, self.max_steering_angle)
-        print("steer after clip :", steering_angle)
+        steering_angle = np.clip(steering_angle, -self.max_steering_angle, self.max_steering_angle)
 
         return steering_angle
 
@@ -128,8 +116,8 @@ class Controller:
     def __init__(self):
         print("Controller __init__ called.")
         self.path = None
-        self.pure_pursuit = PurePursuit(0.0, 0.5)
-        self.pub = rospy.Publisher("/ctrl_cmd", CtrlCmd, queue_size=1)
+        self.pure_pursuit = PurePursuit(0.0, 28.2)
+        self.pub = rospy.Publisher("/lp_ctrl", CtrlCmd, queue_size=1)
         # self.pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
 
         '''
@@ -142,8 +130,9 @@ class Controller:
         float64 acceleration
         '''
         self.path_sub = rospy.Subscriber("/refined_vertices", MarkerArray, self.path_callback)
-        self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback, queue_size=1)
-        
+
+        #self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback, queue_size=1)
+        self.odom_sub = rospy.Subscriber("/Ego", EgoVehicleStatus, self.odom_callback, queue_size=1)
         self.debug_marker = rospy.Publisher("/tmp_goal", Marker, queue_size=1)
         rospy.on_shutdown(self.shutdown)
         
@@ -175,7 +164,6 @@ class Controller:
         print("Path updated.")
         self.path = msg.markers
         self.path_points = [Point(p.points[0].x, p.points[0].y, 0.0) for p in self.path]
-
         # print(self.path)
         #for marker in self.path:
             #print(marker.points[0].x)
@@ -187,19 +175,23 @@ class Controller:
         hours = msg.header.stamp.to_sec() // 3600
         minutes = (msg.header.stamp.to_sec() // 60) % 60
         seconds = msg.header.stamp.to_sec() % 60
-        
-        print("Time : {} hours, {} minutes, {} seconds".format(int(hours), int(minutes), int(seconds)))
+        ego = EgoVehicleStatus()
+        ego.header = msg.header
+        ego.position = msg.position
+        #print("Time : {} hours, {} minutes, {} seconds".format(int(hours), int(minutes), int(seconds)))
         #start = time.time()
     
         morai_cmd = CtrlCmd()
         # ack_cmd = AckermannDriveStamped()
         if self.path == None or len(self.path_points) == 0:
             pass
-        else:
-            orientation_q = msg.pose.pose.orientation
-            orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        else:# 차량의 현재 위치
+            orientation_q = ego
+            orientation_q.heading = math.degrees(orientation_q.heading)
+            # orientation_q = msg.pose.pose.orientation
+            orientation_list = [orientation_q.position.x, orientation_q.position.y, orientation_q.position.z, orientation_q.heading]
             roll, pitch, yaw = euler_from_quaternion(orientation_list)
-            current_pose = [msg.pose.pose.position.x, msg.pose.pose.position.y, yaw]
+            current_pose = [msg.position.x, msg.position.y, yaw]
             # path_points = [Point(p.pose.position.x, p.pose.position.y, 0.0) for p in self.path] # Original code
             self.remove_closest_vertex(current_pose)
             # print(current_pose)
@@ -246,35 +238,35 @@ class Controller:
             -0.5 ~ 0.5 (일거임 아마)
             
             '''
-            # steering limit in Rviz sim
-            min_val = -3.14
-            max_val = 3.14
-            # steering limit in moraiSim
-            norm_min = -0.5
-            norm_max = 0.5
-        
-            morai_cmd.accel = 0.3
+            # # steering limit in Rviz sim
+            # min_val = -3.14
+            # max_val = 3.14
+            # # steering limit in moraiSim
+            # norm_min = -0.5
+            # norm_max = 0.5
 
-            relative_pos = (steering_angle - min_val) / (max_val - min_val)
+            morai_cmd.velocity = 3
 
-            norm_pos = norm_min + relative_pos * (norm_max - norm_min)
+            # relative_pos = (steering_angle - min_val) / (max_val - min_val)
+            #
+            # norm_pos = norm_min + relative_pos * (norm_max - norm_min)
 
-            morai_cmd.steering = norm_pos
-            
+            morai_cmd.steering = -steering_angle
+
             # ack_cmd.drive.speed = 0.55
             # ack_cmd.drive.steering_angle = steering_angle
-            
+
             # In Simulation, cmd.steering range -1.0 ~ 1.0
             # If steering in Python code is 0.1,
             # in simulation, converts -0.2(left)
-            
+
             # cmd.steering = 0.1
             # print(-steering_angle)
-            print("steering_angle")
-            print(morai_cmd.steering)
+            # print("steering_angle")
+            print("실제 제어값 ", morai_cmd.steering)
             print()
             print()
-        
+
             self.pub.publish(morai_cmd)
             # self.pub.publish(ack_cmd)
             rate.sleep()
@@ -294,9 +286,7 @@ class Controller:
         # self.pub.publish(ack_cmd)
         
 if __name__=="__main__":
-        rospy.init_node("Pure_pursuit")
-        try:
-            Controller()
-            rospy.spin()
-        except:
-            pass
+    mct = moraiCtrl()
+    Controller()
+    mct.runCtrl()
+
